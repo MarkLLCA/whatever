40fccacd257fed763ee295a46819b6a62d7cbaf3
nomnomshark <kwangbae39@gmail.com>
2025-04-09 22:44:01 +0800

    fixed some stuffs in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,26 @@
-import Navigation from "./Navigation";
 
+import { Zen_Kaku_Gothic_New } from "next/font/google";
+const zenKakuGothicNewFont = Zen_Kaku_Gothic_New({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-zenkakugothic"
+});
 
 
 export default function Header() {
   return (
     <header className="inset-x-0 fixed top-0 z-10 border-b border-black/5 shadow-xl">
-      <div className="">
-        <div className="bg-transparent flex h-14 items-center justify-between gap-8 px-4 py-[24px]sm:px-6 ">
-          <a href="" className="text-white font-bold focus:outline-none">Library Name</a>
-          <Navigation />
+      <div className={`${zenKakuGothicNewFont.variable}`}>
+        <div className="bg-white flex h-14 items-center justify-between gap-8 px-4 py-[24px]sm:px-6 ">
+          <div className="flex justify-start items-center gap-2">
+            <img src="https://cdn.bulan.sorsu.edu.ph/images/ssu-logo.webp" alt="whatever" className="w-10 h-10" />
+            <a href="" className="text-black font-bold focus:outline-none font-zenkakugothic">SorSU Library Management System</a>
+          </div>
+          <div className="p-[1rem] ml-auto relative">
+            <span className="h-[5px] w-[100%] bg-amber-900 rounded-[25px] absolute top-[50%] left-[50%] transform 
+            -translate-x-1/2 -translate-y-1/2 transition duration-300 ease-in"></span>
+            <span className="h-[5px] w-[100%] bg-amber-900 rounded-[25px] absolute top-[25%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 transition duration-300 ease-in"></span>
+            </div>
         </div>
       </div>
     </header>
